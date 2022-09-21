88c0193f24dddf86d62eb76e73aa3d7cb90b3e2d
polytechnix <polytechnix@yandex.ru>
2022-09-22 00:07:11 +0400

    operations with products in localStorage

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,19 +1,62 @@
 class Products {
 
+	constructor() {
+		this.addedBtnCartText = 'Добавить в корзину';
+		this.removeBtnCartText = 'Удалить из корзины';	
+		this.basicBtnClassName = 'products-element__btn'';
+		this.activeBtnClassName = 'products-element__btn_active'';
+	}
+
+	setLocalStorageStore(element, id) {
+		const { pushProduct, products } = LocalStorage.putProducts(id);
+
+		if(pushProduct) {
+			element.classList.add(this.activeBtnClassName);
+			element.innerHTML = this.removeBtnCartText;
+		} else {
+			element.classList.remove(this.activeBtnClassName);
+			element.innerHTML = this.addedBtnCartText;			
+		}
+
+		// console.log(element, id);
+	}
+
+
 	render() {
 		let htmlCatalog = '';
-		
+		const cartStore = LocalStorage.getProducts();
+
 		CATALOG.forEach(({ id, name, imgSrc, price })  => {
+
+			let btnText = '';
+			let btnClass = '';
+
+			if(cartStore.indexOf(id) === -1) {
+				btnText = this.addedBtnCartText;
+				btnClass = this.basicBtnClassName;
+			} else {
+				btnText = this.removeBtnCartText;
+				btnClass = this.activeBtnClassName;
+			}
 
 			htmlCatalog += `
 				<li class="products-element">
-					<span class="products-element__name">${name}</span>
+					<span class="products-element__name">
+						${name}
+					</span>
+					
 					<img class="products-element__image" src="${imgSrc}" alt="${name}"
-					<span class="products-element__price">${price}</span>
-					<button class="products-element__btn">Добавить в корзину</button>
+					
+					<span class="products-element__price">
+						${price}
+					</span>
+					
+					<button class="${btnClass}" onclick="productsPage.setLocalStorageStore(this, ${id});">
+						${btnText}
+					</button>
 				</li>
 			`;
-		})
+		});
 
 		html += `
 			<ul class="products-container">
