0f581b01fbf74c2625acd739611dc993eb837970
polytechnix <polytechnix@yandex.ru>
2022-09-25 17:18:28 +0400

    added cart component styles and basic styles

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,7 +1,13 @@
 class Cart {
 
+	close-modal() {
+		CART.innerHTML = '';
+	}
+
 	render() {
 		let htmlCatalog = '';
+		let purchaseAmount = 0;
+
 		const cartStore = LocalStorage.getProducts();
 
 		CATALOG.forEach(({ id, name, imgSrc, price })  => {
@@ -9,20 +15,27 @@ class Cart {
 			if(cartStore.indexOf(id) !== -1) {
 				htmlCatalog += `
 					<tr>
-						<td>${imgSrc}</td>
-						<td>${name}</td>
-						<td>${price.toLocaleString()} руб.</td>
+						<td class="cart-item cart-item__img">${imgSrc}</td>
+						<td class="cart-item cart-item__name">${name}</td>
+						<td class="cart-item cart-item__price">${price.toLocaleString()} руб.</td>
 					</tr>
 				`;
+
+				purchaseAmount += price;
 			}
 			
 		});
 
 		const html = `
-			<div class="someClass">
+			<div class="cart-container">
+				
+				<div class="close-btn" onclick="cartPage.close-modal();"></div>
+				
 				<table>
 					${htmlCatalog}
 				</table>
+
+				<p class="amount">Сумма: ${purchaseAmount.toLocaleString()} руб.</p>
 			</div>
 		`;
 
